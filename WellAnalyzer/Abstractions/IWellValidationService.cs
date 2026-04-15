a2087b3b04269c5f4f57e2b6a38a3ea1c1aa4bd6
misha <misha@MIKHAIL>
2026-04-15 17:35:17 +0700

    Добавлен интерфейс для валидации данных по скважине

---
+++ WellAnalyzer/Abstractions/IWellValidationService.cs
@@ -0,0 +1,22 @@
+﻿namespace WellAnalyzer.Abstractions
+{
+	using System.Collections.Generic;
+	using WellAnalyzer.Models;
+
+	/// <summary>
+	/// Интерфейс для сервиса валидации данных по скважине.
+	/// </summary>
+	public interface IWellValidationService
+	{
+		#region Public Methods
+
+		/// <summary>
+		/// Вадириует данные из входящих строк.
+		/// </summary>
+		/// <param name="rows">Список строк.</param>
+		/// <returns>Список ошибок.</returns>
+		List<ValidationError> Validate(List<ImportedWellRow> rows);
+
+		#endregion Public Methods
+	}
+}
