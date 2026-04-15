e63cddce3c9594e419e1aaf010fcea4bb436ab0b
misha <misha@MIKHAIL>
2026-04-15 18:24:47 +0700

    Добавлена вспомогательная модель для результата импорта csxv файла

---
+++ WellAnalyzer/Results/CsvImportResult.cs
@@ -0,0 +1,40 @@
+﻿namespace WellAnalyzer.Results
+{
+	using System.Collections.Generic;
+	using WellAnalyzer.Models;
+
+	/// <summary>
+	/// Результат импорта CSV-файла.
+	/// </summary>
+	public class CsvImportResult
+	{
+		#region Public Properties
+
+		/// <summary>
+		/// Список ошибок при импорте файла.
+		/// </summary>
+		public List<ValidationError> Errors { get; }
+
+		/// <summary>
+		/// Список успешно прочитанных строк.
+		/// </summary>
+		public List<ImportedWellRow> Rows { get; }
+
+		#endregion Public Properties
+
+		#region Public Constructors
+
+		/// <summary>
+		/// Конструктор класса.
+		/// </summary>
+		/// <param name="rows"></param>
+		/// <param name="errors"></param>
+		public CsvImportResult(List<ImportedWellRow> rows, List<ValidationError> errors)
+		{
+			Rows = rows;
+			Errors = errors;
+		}
+
+		#endregion Public Constructors
+	}
+}
