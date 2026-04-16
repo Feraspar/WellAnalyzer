14ac74f689bf0f50ee336e39d531273fcf43ece9
misha <misha@MIKHAIL>
2026-04-17 01:51:54 +0700

    Изменены сущности для промежуточного хранения импортированных строк и ошибок

--- WellAnalyzer/Results/CsvImportResult.cs
+++ WellAnalyzer/Results/CsvImportResult.cs
@@ -6,35 +6,7 @@
 	/// <summary>
 	/// Результат импорта CSV-файла.
 	/// </summary>
-	public class CsvImportResult
-	{
-		#region Public Properties
-
-		/// <summary>
-		/// Список ошибок при импорте файла.
-		/// </summary>
-		public List<ValidationError> Errors { get; }
-
-		/// <summary>
-		/// Список успешно прочитанных строк.
-		/// </summary>
-		public List<ImportedWellRow> Rows { get; }
-
-		#endregion Public Properties
-
-		#region Public Constructors
-
-		/// <summary>
-		/// Конструктор класса.
-		/// </summary>
-		/// <param name="rows">Список успешно прочитанных строк.</param>
-		/// <param name="errors">Список ошибок при импорте файла.</param>
-		public CsvImportResult(List<ImportedWellRow> rows, List<ValidationError> errors)
-		{
-			Rows = rows;
-			Errors = errors;
-		}
-
-		#endregion Public Constructors
-	}
+	/// /// <param name="Rows">Список успешно прочитанных строк.</param>
+	/// <param name="Errors">Список ошибок при импорте файла.</param>
+	public record CsvImportResult(List<ImportedWellRow> Rows, List<ValidationError> Errors);
 }
--- WellAnalyzer/Results/WellValidationResult.cs
+++ WellAnalyzer/Results/WellValidationResult.cs
@@ -6,35 +6,7 @@
 	/// <summary>
 	/// Результат валидации импортированных строк.
 	/// </summary>
-	public class WellValidationResult
-	{
-		#region Public Properties
-
-		/// <summary>
-		/// Список ошибок.
-		/// </summary>
-		public List<ValidationError> Errors { get; }
-
-		/// <summary>
-		/// Валидные строки.
-		/// </summary>
-		public List<ImportedWellRow> ValidRows { get; }
-
-		#endregion Public Properties
-
-		#region Public Constructors
-
-		/// <summary>
-		/// Конструктор класса.
-		/// </summary>
-		/// <param name="validRows">Валидные строки.</param>
-		/// <param name="errors">Список ошибок.</param>
-		public WellValidationResult(List<ImportedWellRow> validRows, List<ValidationError> errors)
-		{
-			ValidRows = validRows;
-			Errors = errors;
-		}
-
-		#endregion Public Constructors
-	}
+	/// /// <param name="ValidRows">Валидные строки.</param>
+	/// <param name="Errors">Список ошибок при валидации.</param>
+	public record WellValidationResult(List<ImportedWellRow> ValidRows, List<ValidationError> Errors);
 }
